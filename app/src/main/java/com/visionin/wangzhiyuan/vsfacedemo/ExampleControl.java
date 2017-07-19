df9d952dc56af0cb621b4f442b9ec45e5e4ff0ce
xiaoxiaopolang <1014008793@qq.com>
2017-07-19 20:12:45 +0800

    release AndroidSDK1.1.0

--- app/src/main/java/com/visionin/wangzhiyuan/vsfacedemo/ExampleControl.java
+++ app/src/main/java/com/visionin/wangzhiyuan/vsfacedemo/ExampleControl.java
@@ -7,6 +7,7 @@ import com.zeroo.lib.Zobject;
 import com.zeroo.lib.gpu.Greators;
 import com.zeroo.lib.gpu.Iraphix;
 import com.zeroo.lib.gpu.Iamera;
+import com.zeroo.lib.gpu.IameraFramer;
 import com.zeroo.lib.media.Mreators;
 import com.zeroo.lib.media.Iapturer;
 
@@ -14,13 +15,12 @@ import com.zeroo.lib.media.Iapturer;
  * Created by wangzhiyuan on 2017/5/17.
  */
 
-public class ExampleControl{
+public class ExampleControl implements IameraFramer {
 	protected Iamera camera = null;
 	protected Iraphix gpu = null;
 
 	protected Iapturer snapshot = null;
-	protected Iapturer capture  = null;
-	//protected ExampleHandler handler = null;
+	protected Iapturer capture = null;
 
 	public ExampleControl(Activity activity) {
 		Zobject.Zactivity = activity;
@@ -29,26 +29,27 @@ public class ExampleControl{
 
 	public void Init(SurfaceView surface) {
 		gpu = Greators.Graphix("G+");                   /// 首先创建 gpu 对象
-		camera = Greators.Camera();                     /// 再创建相机对象
+		gpu.Configurate("GARBAGECOLLECT", 0);
 
-		camera.Mode("facer");                           /// 图像美化+人脸检测
+		camera = Greators.Camera();                     /// 再创建相机对象
+		camera.Mode("facer+render");
 		camera.Preview(surface);                        /// 必须设置预览窗口
-		gpu.Display(true);                              /// 开启预览
+		camera.Display(true);                           /// 开启预览
 
 		snapshot = Mreators.Snapshoter("snapshot.jpg"); /// 创建截图器
-		snapshot.Quality(.5f);                          /// 设置截图质量：0~1
+		snapshot.Quality(.75f);                          /// 设置截图质量：0~1
 		gpu.Snapshoter();                               /// 使用截图器
 
 	///	handler = new ExampleHandler();                 /// 如需自定义的录屏处理，比如推流
 	///	capture = Mreators.Nv12Capturer(handler);       /// 自定义 NV12 帧数据处理器
 	///	capture = Mreators.H264Capturer(handler);       /// 自定义 H264 帧数据处理器（硬编码输出）
 		capture = Mreators.Capturer("capture.mp4");     /// 创建录像机
-		capture.Quality(.5f, 5);                        /// 设置录像质量：0~1 和关键帧间隔（秒数）
-		gpu.Capturer(480, 480);                         /// 使用录像机（设置录像尺寸）
+		capture.Quality(.75f, 5);                        /// 设置录像质量：0~1 和关键帧间隔（秒数）
+		gpu.Capturer(576, 1024);                         /// 使用录像机（设置录像尺寸）
 	}
 
 	public void Display(boolean enable) {
-		gpu.Display(enable);                            /// 开启、关闭预览
+		camera.Display(enable);                         /// 开启、关闭预览
 	}
 
 	public int[] Open() {
@@ -69,23 +70,23 @@ public class ExampleControl{
 
 	public void CaptureOn() {
 		capture.Resume();                               /// 开始录像
-		gpu.Capture();
+		gpu.CaptureStart();
 	}
 
 	public void CaptureOff() {
-		gpu.Pause();                                    /// 停止录像
+		gpu.CaptureStop();                              /// 停止录像
 		capture.Stop();
 	}
 
 	public void FacerOn() {
 		gpu.FacerStart();                               /// 开始人脸检测
-		gpu.FacerMarking(true);                         /// 打开人脸关键点标注，不必要
+		FacerChangeAR(2);
 	}
 
 	/// 停止人脸检测
-	public void FacerOff(){
+	public void FacerOff() {
 		gpu.FacerStop();                                /// 停止人脸检测
-		gpu.FacerMarking(false);                        /// 关闭人脸关键点标注，不必要
+		FacerChangeAR(-1);
 	}
 
 	public void Brightening(float strength) {
@@ -98,5 +99,73 @@ public class ExampleControl{
 
 	public void Sharpening(float strength) {
 		gpu.Parameter("sharpening", strength);          /// 改变锐化程度：0~1
+	}
+
+	public void FacerChangeAR(float strength) {
+		int n = (int)(strength*200-100),
+			d = 1+101/((n < 0) ? 3  : objects.length),
+			i = (strength > 1) ? ar :
+				(strength < 0) ? ar-1 : n/d;
+		if(faceAR) {
+			if(i == ar)
+				return;
+			if(ar > 0)
+				FramerClose(gpu);                       /// 关闭人脸 AR
+			else
+				gpu.FacerMarking(false);                /// 关闭人脸关键点标注
+		}
+		if(strength < 0) {
+			faceAR = false;
+			return;
+		}
+		faceAR = true;
+		ar = i;
+		if(i == 0)
+			gpu.FacerMarking(true);                     /// 打开人脸关键点标注
+		else if(i == -1)
+			gpu.FacerMarking3D(106);                    /// 打开人脸 3D 模型点标注
+		else if(i == -2)
+			gpu.FacerMarking3D(32);                     /// 打开人脸 3D 模型点标注
+		else if(i > 0)
+			FramerOpen(gpu);                            /// 打开人脸 AR
+	}
+
+	protected boolean faceAR = false;
+	protected int ar = 0;
+	protected ExampleRender glasses = new ExampleRender();
+	protected ExampleRender2D dog = new ExampleRender2D();
+	protected IameraFramer[] objects = {null,glasses,dog};
+
+	@Override
+	public boolean FramerOpen(Iraphix gpu) {
+		objects[ar].FramerOpen(gpu);
+		///
+		int selection = camera.Active();
+		camera.OnFramer(this);                          /// 要求每帧同步处理（仅对外部推送纹理必要）
+		gpu.RenderStart(selection == 1);                /// 启动渲染器
+		return true;
+	}
+	@Override
+	public boolean FramerClose(Iraphix gpu) {
+		camera.OnFramer(null);                          /// 取消每帧同步处理
+		gpu.RenderStop();                               /// 关闭渲染器
+		///
+		objects[ar].FramerClose(gpu);
+		return true;
+	}
+	@Override
+	public boolean OnFrameEnter(Iraphix gpu) {
+		objects[ar].OnFrameEnter(gpu);
+		return true;
+	}
+	@Override
+	public boolean OnFrame(Iraphix gpu) {
+		objects[ar].OnFrame(gpu);
+		return true;
+	}
+	@Override
+	public boolean OnFrameExit(Iraphix gpu) {
+		objects[ar].OnFrameExit(gpu);
+		return true;
 	}
 }
